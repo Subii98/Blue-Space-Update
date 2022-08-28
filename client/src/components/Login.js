573d26402c7bd663173b2a2934c62721942367bd
Subii98 <92116531+Subii98@users.noreply.github.com>
2022-08-28 17:35:48 +0900

    modify | client id

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,7 +5,7 @@ import {GoogleLogin} from 'react-google-login';
 // refresh token
 import { refreshTokenSetup } from '../utils/refreshToken';
 
-const clientId = '506755665568-6jjmmjkcpuc4of62a2s5idulrbuebr69.apps.googleusercontent.com';
+const clientId = '862401961471-9rarhcogm291ddfp3q84dqk6cf3heaf6.apps.googleusercontent.com';
 
 
 /*
